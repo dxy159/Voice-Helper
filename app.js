96179798724ef290ff31b934873eb4d423cacc9d
ssLC <chenletaoop@gmail.com>
2017-03-28 20:25:43 -0400

    added modules

--- app.js
+++ app.js
@@ -8,17 +8,32 @@ var bodyParser = require('body-parser');
 
 
 /** --- html to node setup --- */
-var BinaryServer = require('binaryjs').BinaryServer;
-var fs = require('fs');
-var wav = require('wav');
 
+// client connection
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');     // files
+var wav = require('wav');   // sound files
+
+var sqlite3 = require('sqlite').verbose();      // test databases
+var validator = require('email-validator');     // email validator
+var session = require('express-session');       // sessions
+
+
+// global variable for audio file
+// used in binary server to store user audio from UI
+// todo: send to speech to text api to parse
 var outFile = 'demo.wav';
 
 
 
 /** --- generated code --- */
+
+// login page
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+// dashboard page
+var dashboard = require('./routes/dashboard');
 
 var app = express();
 
@@ -36,6 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/dashboard', dashboard);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next){
@@ -57,6 +73,22 @@ app.use(function(err, req, res, next){
 
 /** --- end generated code --- */
 
+
+
+
+/** --- email validation and create session --- */
+app.get('/validate-email', function(req, res){
+
+    // todo: session for user
+
+    // check if email is undefined
+    // and run through validator node module
+    if(req.query.email == undefined || validator.validate(req.query.email) == false){
+        res.send('<p>invalid email, try again</p>');
+    }else{
+        res.end('valid');
+    }
+});
 
 
 /** --- binary server --- */
